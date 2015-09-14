7ec8caf3baecd1afa12e46385013146aa6b646c8
Struan Donald <struan@exo.org.uk>
2015-09-14 13:08:14 +0100

    inital data for members elected under PR
    does not include 11 members elected under majoritarian system

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,50 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# #!/bin/env ruby
+# encoding: utf-8
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri/cached'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  curr_party = ''
+  noko.css('div.contetn-ceter div p').each do |para|
+    party = para.css('b', 'strong').text.to_s
+    if party.index('the electoral list of the') then
+        party = party.gsub(/.*the electoral list of the/, '')
+        party = party.gsub(/^\s*party\s*/, '')
+        party = party.gsub(/[”“″]/, '')
+        party = party.tidy
+        curr_party = party
+        next
+    end
+    if curr_party != '' then
+        possible_name = para.text.to_s.tidy
+        if possible_name.match(/^\d+\./) then
+            name = possible_name.gsub(/^\d+\.\s*/, '').tidy
+
+            data = {
+                name: name,
+                party: party,
+                source: url,
+            }
+
+            ScraperWiki.save_sqlite([:name, :party], data)
+        end
+    end
+  end
+end
+
+scrape_list('http://cecnkr.am/%D5%B6%D5%B8%D6%80%D5%A8%D5%B6%D5%BF%D5%AB%D6%80-%D5%A1%D5%A6%D5%A3%D5%A1%D5%B5%D5%AB%D5%B6-%D5%AA%D5%B8%D5%B2%D5%B8%D5%BE%D5%AB-%D5%BA%D5%A1%D5%BF%D5%A3%D5%A1%D5%B4%D5%A1%D5%BE%D5%B8%D6%80%D5%B6/?lang=en')
